e975f868d650546c6c8ef10b331dc8d0477b8de9
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-07-20 15:29:02 +0300

    Add reactive form to 'add-merchant' component

--- src/app/pages/add-merchant/add-merchant.component.ts
+++ src/app/pages/add-merchant/add-merchant.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'add-merchant',
-  template: `<strong>add merchant form</strong>`,
+  templateUrl: './add-merchant.component.html',
+  styleUrls: ['./add-merchant.component.css'],
 })
-export class AddMerchantComponent {
-  constructor() {}
+export class AddMerchantComponent implements OnInit {
+  addMerchantForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    // private userService: UserService,
+    private router: Router,
+  ) { }
+
+ ngOnInit() {
+    this.addMerchantForm = this.formBuilder.group({
+      merchantName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      adressOne: ['', Validators.required],
+      adressTwo: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      postal: ['', Validators.required],
+      country: ['', Validators.required],
+      phone: ['', Validators.required],
+      vat: ['', Validators.required],
+    });
+  }
+
+  onAdd(): void {
+      /*
+      let formData = this.addMerchantForm.value;
+      let email = formData.email;
+      let password = formData.password;
+      
+      let user = {email, password};
+
+      this.userService.login(user).subscribe(_user => {
+        alert("Congratulations! You are logged in.");
+        this.router.navigate(['/home']);
+      }, (err) => {
+        alert(err);
+        this.router.navigate(['/auth/signup']);
+      });
+      */
+    }
 }
--- src/app/pages/add-merchant/add-merchant.module.ts
+++ src/app/pages/add-merchant/add-merchant.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AddMerchantComponent } from './add-merchant.component';
 import { routing } from './add-merchant.routing';
 
+
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     routing,
   ],
   declarations: [
