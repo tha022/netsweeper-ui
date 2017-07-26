57ab9be1943028cc5cdddc5387f2374fc0f8eb11
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-07-26 18:21:12 +0300

    Add changes to pie-charts in Dashboard

--- src/app/pages/dashboard/pieChart/pieChart.component.ts
+++ src/app/pages/dashboard/pieChart/pieChart.component.ts
@@ -1,18 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-import {PieChartService} from './pieChart.service';
+import { PieChartService } from './pieChart.service';
 
 import 'easy-pie-chart/dist/jquery.easypiechart.js';
 
 @Component({
   selector: 'pie-chart',
   templateUrl: './pieChart.html',
-  styleUrls: ['./pieChart.scss']
+  styleUrls: ['./pieChart.scss'],
 })
 // TODO: move easypiechart to component
 export class PieChart {
 
-  public charts: Array<Object>;
+  charts: Object[];
   private _init = false;
 
   constructor(private _pieChartService: PieChartService) {
