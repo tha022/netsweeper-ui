0e793f75f6bb8375cbe530b8992795c591bfbf63
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-07-31 18:05:48 +0300

    Add changes to manage-merchant table

--- src/app/pages/manage-merchant/manage-merchant.module.ts
+++ src/app/pages/manage-merchant/manage-merchant.module.ts
@@ -1,28 +1,28 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { ManageMerchantComponent } from './manage-merchant.component';
 import { routing } from './manage-merchant.routing';
-import { TablesModule } from '../tables/tables.module';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
-import { NgaModule } from '../../theme/nga.module';
-import { ManageMerchantService } from './manage-merchant.service';
 
+import { MerchantProfileComponent } from './merchant-profile/merchant-profile.component';
+import { ManageMerchantComponent } from './manage-merchant.component';
+
+import { MerchantProfileService } from './merchant-profile/merchant-profile.service';
+import { ManageMerchantService } from './manage-merchant.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    routing,
-    Ng2SmartTableModule,
-    NgaModule,
-    
+    ReactiveFormsModule,
+    routing,   
   ],
   declarations: [
+    MerchantProfileComponent,
     ManageMerchantComponent,
   ],
   providers: [ 
+    MerchantProfileService,
     ManageMerchantService,
   ],
 })
