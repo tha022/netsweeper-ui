86ff21f60ad7f781aea01f1a854af191f66bf2ee
“Thomas <thomas.vervik@seniordevonly.com>
2017-08-01 18:29:20 +0300

    Lot of neat stuff

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: npm start
-worker: json-server --watch db/db.json --routes db/routes.json
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use('/api', proxy('https://netsweeper-db.herokuapp.com'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
