b17d3d3133d4f537ded33038bc7c4b9552f105d9
Dmitry Vlasenko <dvlasenko96@gmail.com>
2017-08-01 13:20:23 +0200

    Revert "[3] add dialog module (#8)" (#9)
    This reverts commit b06d7113ccbe57694b21efd71e4746ce316718ed.

--- src/app/pages/manage-tax-users/manage-tax-users.routing.ts
+++ src/app/pages/manage-tax-users/manage-tax-users.routing.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ManageTaxUserComponent } from './manage-tax-users.component';
 import { TaxUserProfileComponent } from './tax-user-profile/tax-user-profile.component';
+import { ManageComponent } from './manage.component';
 
 const routes: Routes = [
   { path: '', component: ManageTaxUserComponent },
